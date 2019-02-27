dcee0b4e510228e5057ff3ea732864fe095a2684
Joseph Y Cheng <jycheng@stanford.edu>
2019-02-26 17:39:18 -0800

    consolidate logging

--- common.py
+++ common.py
@@ -1,2 +1,9 @@
+import logging
+
 BIN_BART = 'bart'
 FILENAME_PARAMS = 'params.txt'
+
+logger = logging.getLogger('dl-cs')
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT, None))
+logger.addHandler(handler)
