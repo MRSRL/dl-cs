c90f39f9134e1bc2de88d92ad8de975341182de9
Joseph Y Cheng <jycheng@stanford.edu>
2019-02-26 20:26:54 -0800

    add metrics file

---
+++ utils/metrics.py
@@ -0,0 +1,42 @@
+"""Metrics for testing."""
+import numpy as np
+import skimage.measure
+from utils import mri
+
+
+def compute_psnr(ref, x):
+    """Compute peak to signal to noise ratio."""
+    max_val = np.max(np.abs(ref))
+    mse = np.mean(np.square(np.abs(x - ref)))
+    psnr = 10 * np.log(np.square(max_val) / mse) / np.log(10)
+    return psnr
+
+
+def compute_nrmse(ref, x):
+    """Compute normalized root mean square error.
+
+    The norm of reference is used to normalize the metric.
+    """
+    mse = np.sqrt(np.mean(np.square(np.abs(ref - x))))
+    norm = np.sqrt(np.mean(np.square(np.abs(ref))))
+
+    return mse/norm
+
+
+def compute_ssim(ref, x, sos_axis=None):
+    """Compute structural similarity index metric.
+
+    The image is first converted to magnitude image and normalized
+    before the metric is computed.
+    """
+    ref = ref.copy()
+    x = x.copy()
+    if sos_axis is not None:
+        x = mri.sumofsq(x, axis=sos_axis)
+        ref = mri.sumofsq(ref, axis=sos_axis)
+    x = np.squeeze(x)
+    ref = np.squeeze(ref)
+    x /= np.mean(np.square(np.abs(x)))
+    ref /= np.mean(np.square(np.abs(ref)))
+
+    return skimage.measure.compare_ssim(ref, x, data_range=x.max()-x.min())
