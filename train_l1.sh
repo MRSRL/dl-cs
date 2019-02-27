8db6d44ca23e491cd5eba5107053e321733a7a25
Joseph Y Cheng <jycheng@stanford.edu>
2019-02-27 11:03:11 -0800

    use default calib shape

--- train_l1.sh
+++ train_l1.sh
@@ -2,9 +2,8 @@
 python3 recon_train.py \
         --shape_y 320 --shape_z 256 \
         --num_channels 8 --num_maps 1 \
-        --batch_size 6 \
+        --batch_size 2 \
         --model_dir summary/knee_l1 \
-        --shape_calib 10 \
         --loss_l1 1 \
         --max_steps 10000 \
         --device 0
